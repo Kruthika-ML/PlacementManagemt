5e0ecee4f482543be6ad407efbb87a86006dbcb2
Kruthika R <65350577+Kruthika-ML@users.noreply.github.com>
2022-03-11 14:06:05 +0530

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,19 @@
+packpackage com.cg.placementmgmt.client;
+
+import java.time.LocalDate;
+
+public class Client{
+	public static void main(String[] args){
+	IPlacementService service = new IPlacementServiceImpl();
+	Placement placement = new placement();
+
+	placement.setId(3);
+	placement.setName("Kruthika");
+	placement.setCollege("AIT");
+	placement.setQualification("BE");
+	placement.setDate(LocalDate.now());
+	placement.setYear(2022);
+	placement.setPlacement(placement);
+}
+}
+	
